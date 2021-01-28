2ba41b220686b5b0712cfd05b9dade07aa370b76
adzapol <adzapol13@gmail.com>
2021-01-28 14:50:39 +0300

    ver. 0.3.0

--- js/script.js
+++ js/script.js
@@ -40,6 +40,20 @@ let people = [
         age: '27',
     },
     {
+        firstName: 'Запольский',
+        middleName: 'Алексеей',
+        lastName: 'Дмитриевич',
+        floor: 'Мужской',
+        age: '16',
+    },
+    {
+        firstName: 'Запольская',
+        middleName: 'Елена',
+        lastName: 'Игоревна',
+        floor: 'Женский',
+        age: '15',
+    },
+    {
         firstName: 'Шевцов',
         middleName: 'Никита',
         lastName: 'Анатольевич',
@@ -54,6 +68,34 @@ let people = [
         age: '27',
     },
     {
+        firstName: 'Иванова',
+        middleName: 'Нина',
+        lastName: 'Олеговна',
+        floor: 'Женский',
+        age: '19',
+    },
+    {
+        firstName: 'Иванов',
+        middleName: 'Иван',
+        lastName: 'Метрофанович',
+        floor: 'Мужской',
+        age: '20',
+    },
+    {
+        firstName: 'Неструева',
+        middleName: 'Наталья',
+        lastName: 'Вячеславовна',
+        floor: 'Женский',
+        age: '23',
+    },
+    {
+        firstName: 'Алдохин',
+        middleName: 'Вячеслав',
+        lastName: 'Павлович',
+        floor: 'Мужской',
+        age: '21',
+    },
+    {
         firstName: 'Петров',
         middleName: 'Алексей',
         lastName: 'Николаевич',
@@ -66,11 +108,11 @@ let people = [
         lastName: 'Викторовна',
         floor: 'Женский',
         age: '40',
-    },
+    }
 ]
 
-
 function myFunc(array) { // функция вывода людей в контейнер
+
     const sidebar = document.querySelector('.sidebar') // контейнер
     
     for (let i = 0; i < array.length; i++) {
@@ -116,51 +158,97 @@ function myFunc(array) { // функция вывода людей в конте
     }
 }
 
-myFunc(people)
+myFunc(people) // первоначальная загрузка страницы
 
-
-
-
-
-
+// ----------------------------------------
 // Обработчик событий для кнопки "Применить"
+// ----------------------------------------
 
 const btnSubmit = document.querySelector('.btn-sub')
 
+// Фильтр - пол
 let inputMan = document.querySelector('.filter-man')
 let inputWoman = document.querySelector('.filter-woman')
 
-function handleButtonClick() {
+// Фильтр - возраст
+let inputAgeMin18 = document.querySelector('.filter-age-min18')
+let inputAge1820 = document.querySelector('.filter-age-18-20')
+let inputAge2124 = document.querySelector('.filter-age-21-24')
+let inputAgeMore = document.querySelector('.filter-age-more')
+
+// Функция для пола
+function filterFoor() {
     
-    if (inputMan.checked) {
-        console.log('Вывести всех мужчин')
-        
-        const newPeople = []
-        for (let i = 0; i < people.length; i++) {
-            if (people[i].floor == 'Мужской') {
-                newPeople.push(people[i])
-            }
+    let newPeople = people.filter(person => {
+        if (inputMan.checked && person.floor == 'Мужской') {
+            console.log('Вывести всех мужчин')
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.innerHTML = ''
+            return true
+        } else if (inputWoman.checked && person.floor == 'Женский') {
+            console.log('Вывести всех женщин')
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.innerHTML = ''
+            return true
+        } else if (inputMan.checked == false && inputWoman.checked == false) {
+            console.log('Не выбран не один пол')
         }
-        myFunc(newPeople)
-
-
-
-    } else if (inputWoman.checked) {
-        console.log('Вывести всех женщин')
-    } else {
-        console.log('Не выбран не один пол')
-    }
+    })
+    myFunc(newPeople) 
 }
 
-btnSubmit.onclick = handleButtonClick
+// Функция для возраста
+function filterAge() {
+    
+    let newPeople = people.filter(person => {
+        if (inputAgeMin18.checked && person.age < 18) {
+            console.log('Вывести всех людей с возрастом ниже 18 лет')
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.innerHTML = ''
+            return true
+        } else if (inputAge1820.checked && person.age >= 18 && person.age <= 20) {
+            console.log('Вывести всех людей с возрастом от 18 до 20 лет')
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.innerHTML = ''
+            return true
+        } else if (inputAge2124.checked && person.age >= 21 && person.age <= 24) {
+            console.log('Вывести всех людей с возрастом от 21 до 24 лет')
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.innerHTML = ''
+            return true
+        } else if (inputAgeMore.checked && person.age >= 24) {
+            console.log('Вывести всех людей с возрастом более 25 лет')
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.innerHTML = ''
+            return true
+        } else if (inputAgeMin18.checked == false && inputAge1820.checked == false && inputAge2124.checked == false && inputAgeMore.checked == false) {
+            console.log('Не выбран не один возраст')
+        }
+    })
+    myFunc(newPeople) 
+}
 
+btnSubmit.addEventListener('click', filterFoor)
+btnSubmit.addEventListener('click', filterAge)
+
+// ----------------------------------------
 // Обработчик событий для кнопки "Сбросить"
+// ----------------------------------------
 
 const btnReset = document.querySelector('.btn-reset')
 
 function handleButtonReset() {
     inputMan.checked = false
     inputWoman.checked = false
+    inputAgeMin18.checked = false
+    inputAge1820.checked = false
+    inputAge2124.checked = false
+    inputAgeMore.checked = false
+    
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.innerHTML = ''
+    myFunc(people)
+
     console.log('Сброс настроек фильтра')
 }
 
